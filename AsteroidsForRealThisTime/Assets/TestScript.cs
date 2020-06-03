a0aa5c3b8c4a8833bea5c228abdf48f0f2244c1d
stcusick <64747102+stcusick@users.noreply.github.com>
2020-06-03 18:23:04 -0500

    Sean Test Addition
    Testing adding scripts and gameobjects

---
+++ AsteroidsForRealThisTime/Assets/TestScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public float Spin = 0f;
+    float initRot;
+
+    void Start()
+    {
+        initRot = Random.Range(-Spin, Spin);
+    }
+
+    void FixedUpdate()
+    {
+        transform.Rotate(0, 0, initRot);
+    }
+
+}

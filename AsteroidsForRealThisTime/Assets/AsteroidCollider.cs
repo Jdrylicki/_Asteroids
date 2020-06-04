42cefe2bffd2305ba103c6609c9668ce4bdb8583
stcusick <64747102+stcusick@users.noreply.github.com>
2020-06-04 01:55:46 -0500

    Sean did some stuff
    Dinked a little with the asteroid collider and found a way to crash unity

---
+++ AsteroidsForRealThisTime/Assets/AsteroidCollider.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidCollider : MonoBehaviour
+{
+    public GameObject child;
+    public float[] childRot = new float[3];
+    public Vector3[] childPos;
+    public float childScale;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.collider.tag.Equals("Laser"))
+        {
+            Destroy();
+        }
+        
+        /* I need to figure out a way to make this not crash unity lmao
+         * 
+         * 
+         * 
+         * if (collision.collider.tag.Equals("Asteroid"))
+        {
+            Transform tempTransform = transform;
+            Destroy();
+            GameObject asteroid1 = Instantiate(child, childPos[0], Quaternion.Euler(0, 0, childRot[0]), tempTransform);
+            asteroid1.transform.sc .Set(childScale,childScale,1);
+            GameObject asteroid2 = Instantiate(child, childPos[1], Quaternion.Euler(0, 0, childRot[1]), tempTransform);
+            asteroid2.transform.localScale.Set(childScale, childScale, 1);
+            GameObject asteroid3 = Instantiate(child, childPos[2], Quaternion.Euler(0, 0, childRot[2]), tempTransform);
+            asteroid3.transform.localScale.Set(childScale, childScale, 1);
+
+            // instantiate 3 asteroids
+            // set each Asteroid
+        }
+        */
+    }
+
+
+    void Destroy()
+    {
+        Destroy(this.gameObject);
+    }
+
+}
--- AsteroidsForRealThisTime/Assets/AsteroidController.cs
+++ AsteroidsForRealThisTime/Assets/AsteroidController.cs
@@ -7,33 +7,44 @@ public class AsteroidController : MonoBehaviour
     public float maxForce = 10f;
     public float minForce = 1f;
     public float Spin = 0f;
-    float initRot;
+    public float initRot = 0f;
+    private float spinSpeed;
+    public Vector2 initDisp = new Vector2(0, 0);
     public float left, right, lower, upper;
 
     // Start is called before the first frame update
     void Start()
     {
         float initForce = Random.Range(minForce, maxForce);
-        initRot = Random.Range(-Spin, Spin);
-        transform.parent = null;
-        float x = Random.Range(-1f, 1f);
-        float y = Random.Range(-1f, 1f);
-        Vector2 vel = new Vector2(x, y);
-        vel.Normalize();
+        spinSpeed = Random.Range(-Spin, Spin);
+        transform.localPosition.Set(initDisp.x, initDisp.y, 0);
+
+        if (initRot != 0)
+        {
+            transform.Rotate(0, 0, initRot);
+        } else
+        {
+            transform.Rotate(0, 0, spinSpeed * 20);
+        }
+        
+        
         Rigidbody2D tempAsteroid = this.GetComponent<Rigidbody2D>();
-        tempAsteroid.AddForce(vel * initForce);
+        tempAsteroid.AddForce(transform.up * initForce);
     }
 
     void Update()
     {
-        
+        if (transform.parent != null)
+        {
+            transform.parent = null;
+        }
 
 
     }
 
     void FixedUpdate()
     {
-        transform.Rotate(0, 0, initRot);
+        transform.Rotate(0, 0, spinSpeed);
         Remove();
 
     }
